4d05994b565538f276c46061404df591ca8cfded
djanmamur <mmsaidov@gmail.com>
2020-07-19 17:00:06 +0500

    Image model

--- foodcourt/images/models.py
+++ foodcourt/images/models.py
@@ -4,5 +4,5 @@ from ..posts.models import Post
 
 
 class Image(models.Model):
-    post = models.ForeignKey(to=Post, on_delete=models.CASCADE)
+    post = models.ForeignKey(to=Post, related_name="images", on_delete=models.CASCADE)
     url = models.ImageField()
