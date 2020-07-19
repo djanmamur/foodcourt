54ff6a3722956da156e51a168c4f81b9829b63ce
djanmamur <mmsaidov@gmail.com>
2020-07-19 17:00:26 +0500

    Add Post Get and Create views

---
+++ foodcourt/posts/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework.serializers import ModelSerializer, ValidationError
+from ..users.enums import UserType
+from .models import Post
+from ..images.serializers import ImageSerializer
+
+
+class PostSerializer(ModelSerializer):
+    images = ImageSerializer(many=True, read_only=True,)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'name', 'description', 'price', 'images')
+
+
+class CreatePostSerializer(ModelSerializer):
+    def validate_owner(self, owner):
+        if owner.user_type not in UserType.sellers():
+            raise ValidationError("Only sellers can post new items")
+        return owner
+
+    def create(self, validated_data):
+        return Post.objects.create(**validated_data)
+
+    class Meta:
+        model = Post
+        fields = ('id', 'name', 'description', 'price', 'owner',)
