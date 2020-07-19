691e2ad2b1490990e4fa5a90f3af2a0259e22b8a
djanmamur <mmsaidov@gmail.com>
2020-07-19 13:33:19 +0500

    Add Abstract Base Model to handle ID, and Date fields

---
+++ foodcourt/abstract_models.py
@@ -0,0 +1,13 @@
+import uuid
+
+from django.db import models
+
+
+class BaseModel(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    updated = models.DateTimeField(auto_now=True, null=True)
+
+    class Meta:
+        abstract = True
+        db_table = "date_time_model"
